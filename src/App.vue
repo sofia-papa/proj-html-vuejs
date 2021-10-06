2c69119d0423d590c56635f400103ddf220d4530
sofia-papa <papa.sofia@outlook.it>
2021-10-06 12:08:01 +0100

    import bootstrap + creat a new Folder scss

--- src/App.vue
+++ src/App.vue
@@ -22,6 +22,7 @@ export default {
 </script>
 
 <style lang="scss">
+@import './style/variable.scss';
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
---
+++ src/components/Header.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="header-container">
+      <div class="">
+
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
