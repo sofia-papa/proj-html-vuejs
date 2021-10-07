7da92fa8107ca86c909950c20f05b5c5d20e4895
sofia-papa <papa.sofia@outlook.it>
2021-10-07 15:37:30 +0100

    main

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -9,13 +9,37 @@
             </div>
         </div>
       </div>
+
+      <div class="container text-center text-white">
+        <div class="row">
+            <div class="col-12">
+                <h1 class="title">Latest Band News</h1>
+                <hr>
+                <p class="subtitle">Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga consequuntur quibusdam vero, itaque eveniet recusandae exercitationem iure odit, dolores natus sequi, ipsa laborum tenetur reiciendis eos iusto omnis aliquam obcaecati!</p>
+            </div>
+        </div>
+      </div>
+      <div>
+          <Card />
+      </div>
   </div>
 </template>
 
 <script>
+import band from "../data/band.js"
+import Card from './Card.vue'
 
 export default {
-    name: 'Main',
+    name: "band",
+
+    components: {
+        Card,
+    },
+    data: function() {
+        return {
+            products: band,
+        }
+    }
 
 }
 </script >
---
+++ src/data/band.js
@@ -0,0 +1,22 @@
+export default [
+    {
+         photo: "",
+         title: "Technology and music",
+         description: "Sed sit amet sem turpis. Curabitur cursus lacinia est at interdum risus id condimentum.",
+    },
+    {
+        photo: "",
+        title: "While my guitar gently weeps",
+        description: "Sed sit amet sem turpis. Curabitur cursus lacinia est at interdum risus id condimentum.",
+    },
+    {
+        photo: "",
+        title: "It just sounds better",
+        description: "Sed sit amet sem turpis. Curabitur cursus lacinia est at interdum risus id condimentum.",
+    },
+    {
+        photo: "",
+        title: "The flavor of rock",
+        description: "Sed sit amet sem turpis. Curabitur cursus lacinia est at interdum risus id condimentum.",
+    },
+]
